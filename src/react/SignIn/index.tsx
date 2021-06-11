7abcaa2f0751b03ab1fbfd5cc7a4ccc831a29d13
Giorgio Fellipe <giorgiofellipe@gmail.com>
2021-06-11 03:13:27 -0300

    fix(signin): save auth data to local storage and prepare redirection to maps page

--- src/react/SignIn/index.tsx
+++ src/react/SignIn/index.tsx
@@ -1,39 +1,44 @@
-import React, { useState, useCallback }  from 'react';
-import { Button, CardContent, CardHeader } from '@material-ui/core';
+import React, { useState, useCallback } from 'react';
+import { Button, CardContent, CardHeader, ThemeProvider } from '@material-ui/core';
 
 import { StateService } from '@uirouter/core';
 import { Container, Card, CardTitle, CardActions, TextField } from './styles';
 import api from '../../services/api';
+import { reactFractalTheme } from '../../themes';
 
 function SignIn({ state }: { state: StateService }) {
   const [user, setUser] = useState<string>('');
 
-  const handleUserChange = (event: React.ChangeEventHandler<HTMLInputElement>) => {
-    // setUser(event.target.value);
+  const handleUserChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser(e.target.value);
   }
 
-  const handleSubmit = useCallback(() => {
-    api.get('/autenticacao.json', {
+  const handleSubmit = useCallback(async () => {
+    const { data } = await api.get('/autenticacao.json', {
       params: {
         user
       }
     });
 
-    state.go('angular');
+    localStorage.setItem('config', JSON.stringify(data));
+
+    state.go('menu.maps');
   }, [user]);
 
   return (
-    <Container>
-      <Card>
-        <CardContent>
-          <CardHeader title={<CardTitle>Login</CardTitle>}/>
-          <TextField name="user" label="Usuário" onChange={handleUserChange} />
-        </CardContent>
-        <CardActions>
-          <Button type="button" variant="contained" onClick={handleSubmit} color="secondary">Login</Button>
-        </CardActions>
-      </Card>
-    </Container>
+    <ThemeProvider theme={reactFractalTheme}>
+      <Container>
+        <Card>
+          <CardContent>
+            <CardHeader title={<CardTitle>Login</CardTitle>} />
+            <TextField name="user" label="Usuário" onChange={handleUserChange} />
+          </CardContent>
+          <CardActions>
+            <Button type="button" variant="contained" onClick={handleSubmit} color="secondary">Login</Button>
+          </CardActions>
+        </Card>
+      </Container>
+    </ThemeProvider>
   );
 }
 
